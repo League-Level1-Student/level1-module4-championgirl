7ae12a385704cc33a38a6289b2184c6d11e36a80
championgirl <66965981+championgirl@users.noreply.github.com>
2022-08-29 18:25:58 -0700

    ChuckleClicker

---
+++ src/_01_chuckle_clicker/Chuckle_Clicker.java
@@ -0,0 +1,16 @@
+package _01_chuckle_clicker;
+
+public class Chuckle_Clicker {
+
+	public Chuckle_Clicker() {
+		// TODO Auto-generated constructor stub
+	}
+public static void main(String[] args) {
+	Chuckle_Clicker chuckleClicker = new Chuckle_Clicker();
+	chuckleClicker.makeButtons();
+}
+
+public void makeButtons() {
+	
+}
+}
