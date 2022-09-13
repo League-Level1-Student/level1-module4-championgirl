2fba1776dfe5cdfd254300383b825816b21c0a0d
championgirl <66965981+championgirl@users.noreply.github.com>
2022-09-12 18:25:45 -0700

    ChuckleClicker(DONE) Jeopardy(WIP)

--- src/_01_chuckle_clicker/Chuckle_Clicker.java
+++ src/_01_chuckle_clicker/Chuckle_Clicker.java
@@ -1,6 +1,14 @@
 package _01_chuckle_clicker;
 
-public class Chuckle_Clicker {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class Chuckle_Clicker implements ActionListener {
 
 	public Chuckle_Clicker() {
 		// TODO Auto-generated constructor stub
@@ -10,7 +18,49 @@ public static void main(String[] args) {
 	chuckleClicker.makeButtons();
 }
 
+JPanel panel = new JPanel();
+JButton jokeButton = new JButton();
+JButton punchLineButton = new JButton();
+JFrame frame = new JFrame();
 public void makeButtons() {
 	
+	
+	frame.setVisible(true);
+	
+	jokeButton.setText("Joke!");
+	punchLineButton.setText("Punchline!");
+	jokeButton.addActionListener(this);
+	punchLineButton.addActionListener(this);
+	frame.add(panel);
+	panel.add(jokeButton);
+	
+	panel.add(punchLineButton);
+	frame.pack();
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
+@Override
+public void actionPerformed(ActionEvent arg0) {
+	// TODO Auto-generated method stub
+if(arg0.getSource() == jokeButton) {
+		JOptionPane.showMessageDialog(null, "I tripped in France, eiffel over! xD");
+	}
+
+if(arg0.getSource() == punchLineButton) {
+	JOptionPane.showMessageDialog(null, "This would be funny, except there's no punchline xD");
+}
+
+
+}
+
+
+
+
 }
